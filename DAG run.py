b73059977ed8b385e1a61732fba0ad3212d1e12e
Tatiana Tikhonova <tikhonova.t@pm.me>
2022-02-05 12:22:01 -0500

    Added custom operators, included data quality check

--- DAG run.py
+++ DAG run.py
@@ -4,37 +4,23 @@ import logging
 from airflow import DAG
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.python_operator import PythonOperator
+
+from airflow.operators import (
+    HasRowsOperator,
+    PostgresOperator,
+    PythonOperator,
+    S3ToRedshiftOperator
+)
 
 import sql_statements
 
 
-def load_trip_data_to_redshift(*args, **kwargs):
-    aws_hook = AwsHook("aws_credentials")
-    credentials = aws_hook.get_credentials()
-    redshift_hook = PostgresHook("redshift")
-    sql_stmt = sql_statements.COPY_ALL_TRIPS_SQL.format(
-        credentials.access_key,
-        credentials.secret_key,
-    )
-    redshift_hook.run(sql_stmt)
-
-
-def load_station_data_to_redshift(*args, **kwargs):
-    aws_hook = AwsHook("aws_credentials")
-    credentials = aws_hook.get_credentials()
-    redshift_hook = PostgresHook("redshift")
-    sql_stmt = sql_statements.COPY_STATIONS_SQL.format(
-        credentials.access_key,
-        credentials.secret_key,
-    )
-    redshift_hook.run(sql_stmt)
-
-
 dag = DAG(
-    'lesson2.exercise1',
-    start_date=datetime.datetime.now()
+    "updated dag",
+    start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+    end_date=datetime.datetime(2018, 12, 1, 0, 0, 0, 0),
+    schedule_interval="@monthly",
+    max_active_runs=1
 )
 
 create_trips_table = PostgresOperator(
@@ -44,10 +30,23 @@ create_trips_table = PostgresOperator(
     sql=sql_statements.CREATE_TRIPS_TABLE_SQL
 )
 
-copy_trips_task = PythonOperator(
-    task_id='load_trips_from_s3_to_redshift',
+copy_trips_task = S3ToRedshiftOperator(
+    task_id="load_trips_from_s3_to_redshift",
     dag=dag,
-    python_callable=load_trip_data_to_redshift,
+    table="trips",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="data-pipelines/divvy/partitioned/{execution_date.year}/{execution_date.month}/divvy_trips.csv"
+)
+
+
+check_trips = HasRowsOperator(
+    redshift_conn_id="redshift",
+    table="trips",
+    task_id='check_trips_data',
+    dag=dag,
+    provide_context=True,
 )
 
 create_stations_table = PostgresOperator(
@@ -57,11 +56,26 @@ create_stations_table = PostgresOperator(
     sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
 )
 
-copy_stations_task = PythonOperator(
-    task_id='load_stations_from_s3_to_redshift',
+copy_stations_task = S3ToRedshiftOperator(
+    task_id="load_stations_from_s3_to_redshift",
     dag=dag,
-    python_callable=load_station_data_to_redshift,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="data-pipelines/divvy/unpartitioned/divvy_stations_2017.csv",
+    table="stations"
+)
+
+
+check_stations = HasRowsOperator(
+    redshift_conn_id="redshift",
+    table="stations",
+    task_id='check_stations_data',
+    dag=dag,
+    provide_context=True,
 )
 
 create_trips_table >> copy_trips_task
-create_stations_table >>copy_stations_task
+create_stations_table >> copy_stations_task
+copy_stations_task >> check_stations
+copy_trips_task >> check_trips
--- README.md
+++ README.md
@@ -6,6 +6,14 @@
 * airflow
 
 ## Steps
+* Create a Redshift cluster following the below requirements.
 * Create an Airflow connection via Postgres to your AWS cluster; call it 'redshift'.
 * Create an Airflow connection with your AWS User credentials; call it 'aws_credentials'.
 * Launch the Airflow web server to run the DAG.
+
+
+## Cluster requirements
+Since data is hosted on Udacity's S3 @ us-west-2, cluster needs to follow the below criteria:
+* Located in the us-west-2 region
+* Publicly accessible (*open cluster --> actions --> modify to make publicly accessible*)
+* The associated security group must allow the inbound traffic on the default port 5439 (*Cluster Parameters --> Network/Security settings --> VPC security group URL --> add an inbound rule for ALL TRAFIC with 0.0.0.0/0*)
